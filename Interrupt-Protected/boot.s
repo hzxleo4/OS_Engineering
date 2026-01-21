1abde3ea9ec49b084026d6c24be8c1ce6c87fd8f
huang zixuan <hzxleo4@163.com>
2026-01-21 15:55:19 +0800

    hw2

---
+++ Interrupt-Protected/boot.s
@@ -0,0 +1,75 @@
+!	boot.s
+!
+! It then loads the system at 0x10000, using BIOS interrupts. Thereafter
+! it disables all interrupts, changes to protected mode, and 
+! jumps to the beginning of head code at 0x10000 to continue running
+
+BOOTSEG = 0x07c0
+SYSSEG  = 0x1000			! system loaded at 0x10000 (65536).
+SYSLEN  = 32				! sectors occupied.
+
+entry start
+start:
+	jmpi	go,#BOOTSEG
+go:	mov	ax,cs
+	mov	ds,ax
+	mov	es,ax
+
+	mov	ah,#0x03			! Read cursor pos
+	xor	bh,bh				! Character display attribute: page 0
+	int	0x10 				! The BIOS interrupt call 0x10, function 0x03
+	
+	mov	cx,#24				! Number of characters
+	mov	bx,#0x0006			! Character display attribute: page 0, attribute 7 (normal)
+	mov	bp,#msg1			! Point to a string
+	mov	ax,#0x1301			! Write string, move cursor
+	int	0x10 				! The BIOS interrupt call 0x10, function 0x13, subfunc 01
+	
+! ok, we've written the message, now
+load_system:
+	mov	dx,#0x0000
+	mov	cx,#0x0002
+	mov	ax,#SYSSEG
+	mov	es,ax
+	xor	bx,bx
+	mov	ax,#0x200+SYSLEN
+	int 	0x13
+	jnc	ok_load
+die:	jmp	die
+
+! now we want to move to protected mode ...
+ok_load:
+	cli			! no interrupts allowed !
+	lgdt	gdt_48		
+
+	mov	ax,#0x0001	! protected mode (PE) bit
+	lmsw	ax		! This is it!
+	jmpi	0,8		! jmp offset 0 of segment 8 (cs)
+
+msg1:
+	.byte 13,10				! carriage return and line feed (13, 10) characters
+	.ascii "Loading: Hello ..."
+	.byte 13,10,13,10
+
+gdt:	.word	0,0,0,0		! dummy
+
+	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x0000		! base address=0x10000
+	.word	0x9A01		! code read/exec
+	.word	0x00C0		! granularity=4096, 386
+
+	.word	0x07FF		! 8Mb - limit=2047 (2048*4096=8Mb)
+	.word	0x0000		! base address=0x10000
+	.word	0x9201		! data read/write
+	.word	0x00C0		! granularity=4096, 386
+
+	.word   0x0002		! screen 0x18 - for display
+	.word   0x8000
+	.word   0x920b
+	.word   0x00c0
+
+gdt_48: .word	0x7ff		! gdt limit=2048, 256 GDT entries
+	.word	0x7c00+gdt,0	! gdt base = 07xxx
+.org 510
+	.word   0xAA55
+
---
+++ Interrupt-Protected/head.s
@@ -0,0 +1,99 @@
+#  head.s contains the 32-bit startup code.
+#  Two L3 task multitasking. The code of tasks are in kernel area, 
+#  just like the Linux. The kernel code is located at 0x10000. 
+SCRN_SEL	= 0x18
+
+.global startup_32
+.text
+startup_32:
+
+	movl $0x10,%eax 	# 0x10 is the data segment selector in the GDT
+	mov %ax,%ds
+	lss init_stack,%esp
+	
+	# set up gdt and init idt
+	lgdt lgdt_opcode
+	lidt lidt_opcode
+
+	jmp $0x08,$reload	# reload cs by jmp instruction
+reload:
+	movl $0x10,%eax		# reload all the segment registers
+	mov %ax,%ds			# after changing gdt. 
+	mov %ax,%es
+	mov %ax,%fs
+	lss init_stack,%esp
+
+
+	mov $SCRN_SEL, %bx
+	mov %bx,%ds
+	movb $72,2900
+	movb $0x1e,2901
+	movb $69,2902
+	movb $0x1e,2903
+	movb $76,2904
+	movb $0x1e,2905
+	movb $76,2906
+	movb $0x1e,2907
+	movb $79,2908
+	movb $0x1e,2909
+
+	# set idt to handle the exception of protection
+	movl $0x10,%eax	
+	mov %ax,%ds	
+
+	movl $0x00080000, %eax	
+	movw $outofbound_exception, %ax
+	movl $0x8E00, %edx
+	movl $0x0d, %ecx
+	lea idt(,%ecx,8), %esi
+	movl %eax,(%esi) 
+	movl %edx,4(%esi)
+
+	# jump to an illegal address and cpu will trigger an exception
+	jmp 8192
+
+
+outofbound_exception:
+
+	mov $SCRN_SEL, %bx
+	mov %bx,%ds
+	movb $69,3914
+	movb $0x1e,3915
+	movb $82,3916
+	movb $0x1e,3917
+	movb $82,3918
+	movb $0x1e,3919
+	movb $79,3920
+	movb $0x1e,3921
+	movb $82,3922
+	movb $0x1e,3923
+	loop1: jmp loop1
+
+
+lidt_opcode:
+	.word 256*8-1		# idt contains 256 entries
+	.long idt+0x10000	# This will be rewrite by code. 
+
+
+lgdt_opcode:
+	.word (end_gdt-gdt)-1	# the length of gdt 
+	.long gdt+0x10000		# This will be rewrite by code.
+
+
+
+
+idt:	.fill 256,8,0		# idt is uninitialized
+
+gdt:	.quad 0x0000000000000000	/* NULL descriptor */
+		.quad 0x00c09a0100000001	/* 8 KB 0x08, base = 0x10000 */
+		.quad 0x00c0920100000001	/* 8 KB 0x10 */
+		.quad 0x00c0920b80000002	/* screen 0x18 - for display */
+end_gdt:
+
+.fill 128,4,0
+init_stack:                          # Will be used as user stack for task0.
+	.long init_stack
+	.word 0x10
+
+.org 8192
+	loop2: jmp loop2
