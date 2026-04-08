69dea2d61784c05ccf751ff472ee311c8c7d95ed
huang zixuan <hzxleo4@163.com>
2026-04-08 13:01:07 +0800

    test cpp

--- Assign3-Prog/kernel/mytask.c
+++ Assign3-Prog/kernel/mytask.c
@@ -2,17 +2,32 @@
 #include "stdio.h"
 
 void* malloc(u32 size);
+void free(void *ptr);
 
 void main() {
-	int *p1 = malloc(123);
-	if(p1){
-		p1[120] = 123;
-	}
+    int *p1 = (int*)malloc(100);
+    int *p2 = (int*)malloc(200);
 
-	volatile unsigned int *invalid_addr2 = (volatile unsigned int *)0x4100;
-	*invalid_addr2 = 456;
+    if (p1) {
+        p1[0] = 111;
+        p1[1] = 222;
+        printx("p1 ok\n");
+    }
 
-	while (1) {
-		for (int i = 0; i < 4096000; i++) {}  // act as a delay
-	}
+    if (p2) {
+        p2[0] = 333;
+        p2[1] = 444;
+        printx("p2 ok\n");
+    }
+
+    free(p1);
+    printx("free p1 ok\n");
+
+    int *p3 = (int*)malloc(80);
+    if (p3) {
+        p3[0] = 555;
+        printx("p3 reuse ok\n");
+    }
+
+    while (1) { }
 }
